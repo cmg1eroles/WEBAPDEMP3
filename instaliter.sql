02d508c99592265767b8d4bcde5381833e063612
cmg1eroles <cmg1eroles@gmail.com>
2017-08-14 21:09:43 +0800

    db ulit haha

--- instaliter.sql
+++ instaliter.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `instaliter` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `instaliter`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: instaliter
@@ -51,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-08-14 20:37:20
+-- Dump completed on 2017-08-14 21:08:35
